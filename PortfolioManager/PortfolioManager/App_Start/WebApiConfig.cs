daf1a2bec64f3a5f15e79dd2084a382c9d4ce897
Omkar Vasant Manjare <Omkar.Manjare@wu.com>
2019-09-09 12:46:13 +0530

    JWT Token  based authentication.Adding Salt based password mechanism with SHA256 Algorithm

--- PortfolioManager/PortfolioManager/App_Start/WebApiConfig.cs
+++ PortfolioManager/PortfolioManager/App_Start/WebApiConfig.cs
@@ -16,7 +16,7 @@ namespace PortfolioManager
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
--- PortfolioManager/PortfolioManager/Global.asax.cs
+++ PortfolioManager/PortfolioManager/Global.asax.cs
@@ -1,9 +1,16 @@
-﻿using System;
+﻿using Autofac;
+using AutoMapper;
+using PM.BusinessLayer;
+using PM.BusinessLayer.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
+using Autofac.Integration.WebApi;
+using System.Reflection;
+using PM.DataContracts;
 
 namespace PortfolioManager
 {
@@ -12,6 +19,33 @@ namespace PortfolioManager
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            var config = GlobalConfiguration.Configuration;
+            var builder = new ContainerBuilder();//What ContainerBuilder class does/returns
+
+            //Register Web APi Controllers
+            // builder.RegisterApiControllers(Assembly.GetAssembly(typeof (PMDefaultController))); //try this one 
+
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly()); //try this one
+            builder.RegisterType<UserOperations>().As<IUserOperations>().InstancePerRequest();
+            builder.RegisterType<FDOperations>().As<IFDOperations>().InstancePerRequest();
+            // builder.RegisterType<PMOperations>().As<IPMOperations>().InstancePerRequest();
+
+            // builder.RegisterAdapter
+
+
+            var container = builder.Build();//what it does
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
+
+            //Automapper configurations
+            Mapper.Initialize(mapperConfiguration =>
+            {
+                // mapperConfiguration.CreateMap<PMCreateRequest, PMDomainClass>();
+                mapperConfiguration.CreateMap<PMCreateRequest, FixedDeposite>();
+            }
+                                );
+
         }
     }
 }
